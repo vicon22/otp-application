8f089a3dd3ba2460e6fa71e996a906fa253457b2
evgenijisupov <z.isupov@gmail.com>
2025-04-15 20:41:28 +0300

    fix security commit

--- src/main/java/com/eveiled/otp/util/JwtUtil.java
+++ src/main/java/com/eveiled/otp/util/JwtUtil.java
@@ -1,13 +1,12 @@
 package com.eveiled.otp.util;
 
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -20,38 +19,42 @@ public class JwtUtil {
     private long expirationMs;
 
     public String generateToken(String username, String role) {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        Key key = Keys.hmacShaKeyFor(keyBytes); // безопасно и корректно
+
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS512)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
-
     }
 
     public String extractUsername(String token) {
-        return Jwts.parser().setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+        return parseToken(token).getBody().getSubject();
+    }
+
+    public String extractRole(String token) {
+        return parseToken(token).getBody().get("role", String.class);
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            parseToken(token);
             return true;
         } catch (JwtException e) {
             return false;
         }
     }
 
-    public String extractRole(String token) {
-        return Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody()
-                .get("role", String.class);
+    private Jws<Claims> parseToken(String token) {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
     }
 }
-
